c0e53fe606b8bed3340eb716ddc16d553c57a209
Iype Eldho <62381123+viper-2000@users.noreply.github.com>
2023-02-27 16:18:53 -0500

    Added MADA code
    MADA - contains network classes and evaluation functions
    train_func - contains datapath initialization, getting the data, normalizing data, and optimizing function
    Office_31_train - implements the above two files on Office-31 dataset

---
+++ MADA/Office_31_train.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/9/11 10:20
+# @Author : CHT
+# @Site : 
+# @File : train.py
+# @Software: PyCharm 
+# @Blog: https://www.zhihu.com/people/xia-gan-yi-dan-chen-hao-tian
+# @Function:
+
+
+import csv
+import MADA
+import train_func
+import os
+
+root_path = train_func.Office_31_root_path
+feas_dim = 2048
+n_classes = 31
+batch_size = 256
+encoder_out_dim = 512
+
+def train_to_csv(ds, dt, iterations=5, fea_type='Resnet50', n_Dtl=0.03):
+    domain_name = '{}_{}'.format(ds[:-4], dt[:-4])
+    result_path = r'F:\Python_project\Experimental_Result\Office_31\MADA\911'
+    os.makedirs(result_path, exist_ok=True)
+
+    # # Get features and labels
+    # feas_ds, labels_ds = get_data.get_feas_labels(root_path, ds, fea_type=fea_type)
+    # feas_dt, labels_dt = get_data.get_feas_labels(root_path, dt, fea_type=fea_type)
+    # # Get dataloaders
+    # dataloader_dt = get_data.get_src_dataloader_by_feas_labels(
+    #     feas_dt, labels_dt, batch_size=batch_size, normalization=True, fea_type=fea_type)
+    # dataloader_ds = get_data.get_src_dataloader_by_feas_labels(
+    #     feas_ds, labels_ds, batch_size=batch_size, normalization=True, fea_type=fea_type)
+
+    dataloader_ds, dataloader_dt = train_func.get_sd_td_with_labels_dataloader(
+        root_path, ds, dt, fea_type=fea_type, n_Dtl=n_Dtl, batch_size=batch_size)
+    print('{}, Ds:{}, Dt:{}'.format(domain_name, len(dataloader_ds.dataset), len(dataloader_dt.dataset)))
+
+    train_epoch = 500
+    domain_labels = [0.0, 1.0]
+    loss_weights = [1, 0.5]
+    dataloaders = [dataloader_ds, dataloader_dt]
+
+    for _ in range(iterations):
+
+        mada_net = MADA.MADA_networks(in_dim=feas_dim, n_classes=n_classes, encoder_out_dim=encoder_out_dim).cuda()
+
+        acc_tgt_best = MADA.training(
+            dataloaders, mada_net, train_epoch, loss_weights, n_classes, lr=1e-2)
+
+        with open(r'{}\{}.csv'.format(result_path, domain_name), 'a+',
+                  newline='') as f:
+            f_csv = csv.writer(f)
+            f_csv.writerow([float(acc_tgt_best)])
+
+
+
+
+if __name__ == '__main__':
+
+    train_to_csv(train_func.domain_a, train_func.domain_ad)
+    train_to_csv(train_func.domain_a, train_func.domain_aw)
+
+    train_to_csv(train_func.domain_d, train_func.domain_da)
+    train_to_csv(train_func.domain_w, train_func.domain_wa)
+
+    train_to_csv(train_func.domain_d, train_func.domain_dw)
+    train_to_csv(train_func.domain_w, train_func.domain_wd)

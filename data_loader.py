7c2f964b2e528e4fa64cc81ab565d4760f1b5d2c
gmvincent <gmvincen@ncsu.edu>
2023-02-26 23:47:23 -0500

    updated dataloader to remove bugs

--- data_loader.py
+++ data_loader.py
@@ -8,75 +8,111 @@ import matplotlib.pyplot as plt
 import torch
 import torchvision.transforms as transforms
 
+classes = [
+    "back_pack",
+    "bike",
+    "bike_helmet",
+    "bookcase",
+    "bottle",
+    "calculator",
+    "desk_chair",
+    "desk_lamp",
+    "desktop_computer",
+    "file_cabinet",
+    "headphones",
+    "keyboard",
+    "laptop_computer",
+    "letter_tray",
+    "mobile_phone",
+    "monitor",
+    "mouse",
+    "mug",
+    "paper_notebook",
+    "pen",
+    "phone",
+    "printer",
+    "projector",
+    "punchers",
+    "ring_binder",
+    "ruler",
+    "scissors",
+    "speaker",
+    "stapler",
+    "tape_dispenser",
+    "trash_can",
+]
+
+
 class Amazon(torch.utils.data.Dataset):
-    def __init__(self, path, source=True, transforms=None):
+    def __init__(self, path, source=True, transforms=None, batch_size=32):
         self.path = path
-        self.files = glob(os.path.join(path, "**", "*.jpg"))
+        self.files = glob(os.path.join(path, "**", "*.jpg"), recursive=True)
         self.source = source
         self.transforms = transforms
-        
+
     def __len__(self):
         return len(self)
-    
+
     def __getitem__(self, idx):
-        label = 0 # if source (unsupervised training) dummy label
-        
+        label = 0  # if source (unsupervised training) dummy label
+
         file = self.files[idx]
         img = Image.open(file)
-        
+
         if self.source == False:
-            label = file.lstrip(self.path).split("/")[0]
-        
+            label = file.split(self.path)[-1].split("/images/")[-1].split("/")[0]
+
         if self.transforms is not None:
             img = self.transforms(img)
 
-        
         return img, label
-    
+
+
 class Webcam(torch.utils.data.Dataset):
-    def __init__(self, path, source=True, transforms=None):
+    def __init__(self, path, source=True, transforms=None, batch_size=32):
         self.path = path
-        self.files = glob(os.path.join(path, "**", "*.jpg"))
+        self.files = glob(os.path.join(path, "**", "*.jpg"), recursive=True)
         self.source = source
         self.transforms = transforms
-        
+
     def __len__(self):
         return len(self)
-    
+
     def __getitem__(self, idx):
-        label = 0 # if source (unsupervised training) dummy label
-        
+        label = 0  # if source (unsupervised training) dummy label
+
         file = self.files[idx]
         img = Image.open(file)
-        
+
         if self.source == False:
-            label = file.lstrip(self.path).split("/")[0]
-        
+            label = file.split(self.path)[-1].split("/images/")[-1].split("/")[0]
+
         if self.transforms is not None:
             img = self.transforms(img)
 
         return img, label
-    
+
+
 class DSLR(torch.utils.data.Dataset):
-    def __init__(self, path, source=True, transforms=None):
+    def __init__(self, path, source=True, transforms=None, batch_size=32):
         self.path = path
-        self.files = glob(os.path.join(path, "**", "*.jpg"))
+        self.files = glob(os.path.join(path, "**", "*.jpg"), recursive=True)
         self.source = source
         self.transforms = transforms
-        
+
     def __len__(self):
         return len(self)
-    
+
     def __getitem__(self, idx):
-        label = 0 # if source (unsupervised training) dummy label
-        
+        label = 0  # if source (unsupervised training) dummy label
+
         file = self.files[idx]
         img = Image.open(file)
-        
+
         if self.source == False:
-            label = file.lstrip(self.path).split("/")[0]
-        
+            label = file.split(self.path)[-1].split("/images/")[-1].split("/")[0]
+
         if self.transforms is not None:
             img = self.transforms(img)
-  
+
         return img, label
